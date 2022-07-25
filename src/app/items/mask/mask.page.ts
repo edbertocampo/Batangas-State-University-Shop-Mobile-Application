8e02df8d9d859db180c07d746ea352f4e6063f26
jxblrkrstffrbrms <jaxblairekristofferramos@gmail.com>
2022-07-25 20:39:53 +0800

    added contents on the item pages

--- src/app/items/mask/mask.page.ts
+++ src/app/items/mask/mask.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-mask',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MaskPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  home(){
+    this.router.navigate(['/tabs'])
   }
 
 }
